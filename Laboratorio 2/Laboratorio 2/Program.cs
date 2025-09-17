419acd906a1babc3155c1a46b0f5f0aac1764b2f
Joalesf <josealesanchez22@gmail.com>
2025-09-16 19:31:16 -0500

    Subiendo en Laboratorio #2 Ejemplo 1 - Variables de instancias

---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio_2 //Laboratorio2 José Sánchez - Variables de Instancia
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.FirstName = "José ";
+        client.LastName = "Sánchez";
+        client.Age = 19;
+        client.Id = 1;
+
+        Console.WriteLine(client.GetFullName());
+    }
+}
+}
+
+public class Client
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string GetFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+}
