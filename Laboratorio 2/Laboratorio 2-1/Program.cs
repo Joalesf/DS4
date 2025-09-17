3644e6801197f785b26172107365c3cecaa231d7
Joalesf <josealesanchez22@gmail.com>
2025-09-16 19:45:05 -0500

    Creando el Laboratorio 2-1 usando una varibale estática.

---
+++ Laboratorio 2/Laboratorio 2-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laboratorio_2_1 //Laboratorio2.1 José Sánchez 
+{
+    public class Program
+    {
+        static void Main()
+        {
+        //Variable Static
+        MyClass.Valor = 10; //Cambie el valor de la variable estática
+        Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        public static int Valor;
+    }
+}
