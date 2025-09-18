f8eaf05970a8abb34afde99ab6c6a39430d6a09f
Joalesf <josealesanchez22@gmail.com>
2025-09-17 19:37:09 -0500

    Subiendo el Laboratorio 3-1 (a-b)

---
+++ Laboratorio 3/Laboratorio 3-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio_3_1
+{
+    
+    class CalculosMatematicos
+    {
+        public int Calcular(int a, int b)
+        {
+            return (a + b) * (a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número (a): ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número (b): ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            CalculosMatematicos calculos = new CalculosMatematicos();
+            int resultado = calculos.Calcular(a, b);
+
+            Console.WriteLine("El resultado de la operación (a+b)*(a-b) es: " + resultado);
+        }
+    }
+}
