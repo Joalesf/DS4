06ec550e51690bbe13dd87ab3ddfd5df00f601e0
Joalesf <josealesanchez22@gmail.com>
2025-09-17 20:11:28 -0500

    Subiendo el ejmplo de el laboratorio 4.

---
+++ Laboratorio 4/Laboratorio 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laboratorio_4;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 1;
+
+        while (x <= 100)
+        {
+            Console.WriteLine(x);
+            x = x + 1;
+        }
+
+        Console.WriteLine("Fin del programa");
+    }
+}
