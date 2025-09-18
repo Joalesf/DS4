09ab9c9a981ff245722b483c8b9e3b8f24005ba4
Joalesf <josealesanchez22@gmail.com>
2025-09-18 00:56:24 -0500

    Lab4-7 Suma de numero con un  static double Suma

---
+++ Laboratorio 4/Laboratorio 4-7/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroUno = 70;
+        double numeroDos = 67.89;
+        double numeroTres = 67.89;
+
+        Console.WriteLine(Suma(numeroUno, numeroDos)); //solo a 2 decimales
+        Console.WriteLine(Suma(numeroUno, numeroDos, numeroTres));// da mas de 4 ecimales, pero porque sucede esto?
+
+        //2 sumas realizadas
+    }
+
+    static double Suma(int x, double y, double z = 0) // se crea el metodo "creo" e suma, para que sea valido y lo pueda leer
+    {
+        return x + y + z;
+    }
+}
