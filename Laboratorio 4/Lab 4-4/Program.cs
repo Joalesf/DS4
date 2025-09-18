3308aadbb644fcd7356d4ebed0ca40da3481c579
Joalesf <josealesanchez22@gmail.com>
2025-09-18 00:20:58 -0500

    Subiendo el Lab4-4 Condicionales

---
+++ Laboratorio 4/Lab 4-4/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la nota el estudiante");
+        float score = float.Parse(Console.ReadLine());
+
+        if (score >= 70)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Su nota es: {score} ha aprobado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Su nota es: {score} ha reprobado, debe repetir");
+        }
+    }
+}
