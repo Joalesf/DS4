f32d8c3560a5a7d4b2341b3d43f2c5e55d863aa1
Joalesf <josealesanchez22@gmail.com>
2025-09-17 19:52:25 -0500

    Terminado y subiendo todo los cambios del laboratorio #3

---
+++ Laboratorio 3/Laboratorio 3-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Laboratorio_3_2
+{
+    class Circulo
+    {
+        public double CalculoArea(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo c = new Circulo();
+
+            Console.Write("Ingrese el radio del círculo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            double area = c.CalculoArea(radio);
+
+            Console.WriteLine("El área del círculo es: " + area);
+
+            Console.WriteLine("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio 3/Laboratorio 3-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Laboratorio_3_3
+{
+    class Rectangulo
+    {
+        public double CalculoPerimetro(double largo, double ancho)
+        {
+            return 2 * (largo + ancho);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo r = new Rectangulo();
+
+            Console.Write("Ingrese el largo del rectángulo: ");
+            double largo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el ancho del rectángulo: ");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = r.CalculoPerimetro(largo, ancho);
+
+            Console.WriteLine("El perímetro del rectángulo es: " + perimetro);
+
+            Console.WriteLine("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
