72cf61117413151a06983d2ab05913ec55a74e9a
Joalesf <josealesanchez22@gmail.com>
2025-09-23 19:57:32 -0500

    Subiendo Laboratorio 5-3 uso de Foreach en arreglos y matrices

---
+++ Laboratorio 5/Laboratorio 5-3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+                //José Sánchez 8-1032-2111
+{
+    private static void Main(string[] args)
+    {
+        string[] frutas = { "Manzana", "Plantano", "Naranja", "Jose" };
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
