693806df0d4713397f4424469e20fe3538297def
Joalesf <josealesanchez22@gmail.com>
2025-10-01 20:49:38 -0500

    Subiendo Lab 9 , problema #1, Precio de Producto

---
+++ Laboratorio 9/Laboratorio 9-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int precio;
+        string forma_depago;
+
+        Console.WriteLine("Ingrese el precio del producto:");
+        precio = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la forma de pago (Efectivo, Tarjeta):");
+        forma_depago = Console.ReadLine();
+
+        switch (forma_depago)
+        {
+            case "Efectivo":
+                precio = precio;
+                Console.WriteLine("El precio es: " + precio);
+                break;
+
+            case "Tarjeta":
+                Console.WriteLine("Ingrese el número de tarjeta:");
+                string numeroTarjeta = Console.ReadLine();
+
+                precio = precio;
+                Console.WriteLine("El precio es: " + precio);
+                break;
+
+            default:
+                Console.WriteLine("Forma de pago no válida.");
+                break;
+        }
+    }
+}
