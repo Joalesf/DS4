cd55428c64a81c8b1bc3542dfb58c92e357f9889
Joalesf <josealesanchez22@gmail.com>
2025-10-02 03:02:07 -0500

    Subiendo Laboratorio 9-4

---
+++ Laboratorio 9/Laboratorio 9-4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laboratorio_9_4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int numero = Aleatorio.generar(1, 100);
+            Console.WriteLine("Número aleatorio: " + numero);
+
+            int[] numeros = Aleatorio.arreglo(5, 1, 50);
+            Console.WriteLine("Arreglo generado: " + string.Join(", ", numeros));
+        }
+    }
+}
