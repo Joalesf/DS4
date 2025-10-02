0ac579cfcf5fbd762d48d1357bd48875c8bb412b
Joalesf <josealesanchez22@gmail.com>
2025-10-01 21:03:12 -0500

    Subiendo Laboratorio 9-3, TRIANGULOS

---
+++ Laboratorio 9/Laboratorio 9-3/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int lado1, lado2, lado3;
+
+        Console.WriteLine("Ingrese el primer lado del triángulo:");
+        lado1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo lado del triángulo:");
+        lado2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el tercer lado del triángulo:");
+        lado3 = Convert.ToInt32(Console.ReadLine());
+
+        if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es EQUILÁTERO.");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es ISÓSCELES.");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es ESCALENO.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Los lados ingresados NO forman un triángulo válido.");
+        }
+    }
+}
