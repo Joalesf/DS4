288a217eb7971130a18081e1eab90b1dad04b490
Joalesf <josealesanchez22@gmail.com>
2025-10-03 04:40:55 -0500

    Subiendo Lab-8-8 Clases Abstractas

---
+++ Laboratorio 8/Laboratorio 8-8/ClaseConcreta2.cs
@@ -0,0 +1,12 @@
+﻿    class ClaseConcreta2 : ClaseAbstracta
+    {
+        protected override string tomarValor()
+        {
+            return "ClaseConcreta2";
+        }
+
+        public override string prefixValor(string prefix)
+        {
+            return $"{prefix}ClaseConcreta2";
+        }
+    }
