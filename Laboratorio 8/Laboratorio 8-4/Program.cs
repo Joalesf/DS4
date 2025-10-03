558d02b3e2fd5376db240f19ff3eb64bf55674f8
Joalesf <josealesanchez22@gmail.com>
2025-10-03 04:11:52 -0500

    Subiendo LAB 8-4 encapsulado con propiedaes

---
+++ Laboratorio 8/Laboratorio 8-4/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Empleado empleado= new Empleado();
+        empleado.Nombre = "John Doe";
+        Console.WriteLine($"Nombre del empleado: {empleado.Nombre}");
+
+        CuentaBancaria cuenta = new CuentaBancaria();
+        cuenta.Saldo = 100;
+        Console.WriteLine($"El saldo de el empleado es: {cuenta.Saldo}");
+        //Probe con saldo -100 y me lanza la excepcion correctamente 
+
+        Cobertura c = new Cobertura(5);
+        Console.WriteLine($"Con una covertura de: {c.Radio}");
+    }
+}
