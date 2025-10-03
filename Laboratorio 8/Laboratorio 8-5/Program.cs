3f01ef94a4050a1cb32dc3513eb6e292db17aea4
Joalesf <josealesanchez22@gmail.com>
2025-10-03 04:17:21 -0500

    PARTE 5 – PARTIAL CLASS Dividir una clase por partes, esto cuando se hace el código extenso

---
+++ Laboratorio 8/Laboratorio 8-5/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Coordenadas coordenadas = new Coordenadas(10,15);
+        coordenadas.VerCoordenadas();
+    }
+}
