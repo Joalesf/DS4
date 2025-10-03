85bdd120de5e8e7dbbeb7c0ce7833b08439d4730
Joalesf <josealesanchez22@gmail.com>
2025-10-03 04:34:01 -0500

    PARTE 7 – CLASE ABSTRACTA , Corrigiendo sealed

---
+++ Laboratorio 8/Laboratorio 8-7/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Funcionó la app");
+    }
+}
+
+class ClaseBase // se elimina sealed porque abajo se hereda
+{
+    public void test() 
+    { 
+
+    }
+
+    public void moreTesting()
+    {
+
+    }
+}
+class ClaseHijo : ClaseBase
+{
+
+}
