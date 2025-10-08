3ef1e19d17a4c65e83648dd8a364cfb5edd5815e
Joalesf <josealesanchez22@gmail.com>
2025-10-07 23:26:06 -0500

    Subiendo Laboratorio 10 (1)

---
+++ Laboratorio 10-1/Laboratorio 10-1/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+'José Sánchez 8-1032-211'
+
+Module Module1 'A diferencia de C# que usa namespace
+    Sub Main()
+        Console.WriteLine("Hola")
+        Console.WriteLine("Este es mi primer programa de consola de Visual Basic")
+    End Sub
+End Module
---
+++ Parcial-1/Parcial-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Matriz A :");
+        int a = int.Parse(Console.ReadLine());
+
+        int b;
+        do
+        {
+            Console.WriteLine("Matriz B :");
+            b = int.Parse(Console.ReadLine());
+        } while (b % 2 == 0);
+
+        int[,] matriz = new int[a, b];
+        Random numero = new Random();
+
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                if (j % 2 != 0) // columnas impares
+                {
+                    int valor;
+                    do
+                    {
+                        valor = numero.Next(101, 200); // entre 101 y 199
+                    } while (valor % 2 == 0); // asegurar que sea impar
+                    matriz[i, j] = valor;
+                }
+                else
+                {
+                    matriz[i, j] = 0; // columnas pares quedan en 0
+                }
+            }
+        }
+
+        Console.WriteLine("\nImpresión de la matriz:");
+
+        for (int i = 0; i < a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                Console.Write(matriz[i, j] + (j + 1 == b ? "\n" : " - "));
+            }
+        }
+
+        Console.WriteLine($"Muliplicacion de  aleatorios de la segunda columna: {matriz[0, 1] * matriz[a - 1, 1]}");
+
+        Console.ReadKey(true);
+    }
+}
