73cf62069a5cc325f6f0b617dac8925761d8391e
Joalesf <josealesanchez22@gmail.com>
2025-10-15 20:11:37 -0500

    subiendo lab 13-1 en esta laboratorio integre lo que se me pidio y concete la base de datos e hice un boton para salir.

---
+++ Laboratorio 13/Laboratorio 13-1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Laboratorio_13_1
+{
+    partial class Ventana
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conexion = new System.Windows.Forms.Button();
+            this.listBox_Nortwhid = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_conexion
+            // 
+            this.btn_conexion.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_conexion.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_conexion.Location = new System.Drawing.Point(168, 39);
+            this.btn_conexion.Name = "btn_conexion";
+            this.btn_conexion.Size = new System.Drawing.Size(190, 73);
+            this.btn_conexion.TabIndex = 0;
+            this.btn_conexion.Text = "Conectar y Desconectar de Sql Server";
+            this.btn_conexion.UseVisualStyleBackColor = false;
+            this.btn_conexion.Click += new System.EventHandler(this.btn_conexion_Click);
+            // 
+            // listBox_Nortwhid
+            // 
+            this.listBox_Nortwhid.FormattingEnabled = true;
+            this.listBox_Nortwhid.ItemHeight = 16;
+            this.listBox_Nortwhid.Location = new System.Drawing.Point(137, 175);
+            this.listBox_Nortwhid.Name = "listBox_Nortwhid";
+            this.listBox_Nortwhid.Size = new System.Drawing.Size(266, 180);
+            this.listBox_Nortwhid.TabIndex = 1;
+            this.listBox_Nortwhid.SelectedIndexChanged += new System.EventHandler(this.listBox_Nortwhid_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Ventana
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 402);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox_Nortwhid);
+            this.Controls.Add(this.btn_conexion);
+            this.Name = "Ventana";
+            this.Text = "Base de Datos";
+            this.Load += new System.EventHandler(this.Ventana_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conexion;
+        private System.Windows.Forms.ListBox listBox_Nortwhid;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Laboratorio 13/Laboratorio 13-1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_13_1
+{
+    public partial class Ventana : Form
+    {
+        string connectionString =
+        @"Server=JOALEXS;Database=Northwind;TrustServerCertificate=True;Integrated Security=True;";
+        public Ventana()
+        {
+            InitializeComponent();
+        }
+        private void btn_conexion_Click(object sender, EventArgs e)
+        {
+            
+                SqlConnection conexion = new SqlConnection(connectionString);
+                conexion.Open();
+                MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+                conexion.Close();
+                MessageBox.Show("Se cerró la conexión.");
+        }
+        private void Ventana_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT ProductName FROM [dbo].[Products]";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+
+            {
+                SqlCommand comando = new SqlCommand(query, conexion);
+                conexion.Open();
+
+                SqlDataReader lector = comando.ExecuteReader();
+
+                listBox_Nortwhid.Items.Clear();
+
+                while (lector.Read())
+                {
+                    listBox_Nortwhid.Items.Add(lector["ProductName"].ToString());
+                }
+
+                lector.Close();
+            }
+        }
+        private void listBox_Nortwhid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
