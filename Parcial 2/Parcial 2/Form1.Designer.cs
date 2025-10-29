d022b36e33aa02469d9b228b8088b7a70d896742
Joalesf <josealesanchez22@gmail.com>
2025-10-29 10:45:02 -0500

    Subiendo ultima actualización

---
+++ Parcial 2/Parcial 2/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Parcial_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TITULO = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEur = new System.Windows.Forms.TextBox();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.btnConversion1 = new System.Windows.Forms.Button();
+            this.txtConversion2 = new System.Windows.Forms.Button();
+            this.txtConversion3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEuroxeuro = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEuroxDolar = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEuroxPeso = new System.Windows.Forms.TextBox();
+            this.txtDolarxPeso = new System.Windows.Forms.TextBox();
+            this.txtDolarxDolar = new System.Windows.Forms.TextBox();
+            this.TxtDolarxEuro = new System.Windows.Forms.TextBox();
+            this.TxtPesoXPeso = new System.Windows.Forms.TextBox();
+            this.txtPesoxDolar = new System.Windows.Forms.TextBox();
+            this.txtPesoxEuro = new System.Windows.Forms.TextBox();
+            this.SUBT = new System.Windows.Forms.Label();
+            this.Mostar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TITULO
+            // 
+            this.TITULO.AutoSize = true;
+            this.TITULO.Location = new System.Drawing.Point(245, 9);
+            this.TITULO.Name = "TITULO";
+            this.TITULO.Size = new System.Drawing.Size(87, 16);
+            this.TITULO.TabIndex = 0;
+            this.TITULO.Text = "COTIZACIÓN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Euro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dólar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Peso";
+            // 
+            // txtEur
+            // 
+            this.txtEur.Location = new System.Drawing.Point(104, 92);
+            this.txtEur.Name = "txtEur";
+            this.txtEur.Size = new System.Drawing.Size(100, 22);
+            this.txtEur.TabIndex = 4;
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(104, 137);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(100, 22);
+            this.txtDolar.TabIndex = 5;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(104, 186);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 22);
+            this.txtPeso.TabIndex = 6;
+            // 
+            // btnConversion1
+            // 
+            this.btnConversion1.Location = new System.Drawing.Point(210, 92);
+            this.btnConversion1.Name = "btnConversion1";
+            this.btnConversion1.Size = new System.Drawing.Size(46, 23);
+            this.btnConversion1.TabIndex = 7;
+            this.btnConversion1.Text = "=";
+            this.btnConversion1.UseVisualStyleBackColor = true;
+            // 
+            // txtConversion2
+            // 
+            this.txtConversion2.Location = new System.Drawing.Point(210, 136);
+            this.txtConversion2.Name = "txtConversion2";
+            this.txtConversion2.Size = new System.Drawing.Size(46, 23);
+            this.txtConversion2.TabIndex = 8;
+            this.txtConversion2.Text = "=";
+            this.txtConversion2.UseVisualStyleBackColor = true;
+            // 
+            // txtConversion3
+            // 
+            this.txtConversion3.Location = new System.Drawing.Point(210, 186);
+            this.txtConversion3.Name = "txtConversion3";
+            this.txtConversion3.Size = new System.Drawing.Size(46, 23);
+            this.txtConversion3.TabIndex = 9;
+            this.txtConversion3.Text = "=";
+            this.txtConversion3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(274, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Euro";
+            // 
+            // txtEuroxeuro
+            // 
+            this.txtEuroxeuro.Location = new System.Drawing.Point(262, 95);
+            this.txtEuroxeuro.Name = "txtEuroxeuro";
+            this.txtEuroxeuro.Size = new System.Drawing.Size(70, 22);
+            this.txtEuroxeuro.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Dólar";
+            // 
+            // txtEuroxDolar
+            // 
+            this.txtEuroxDolar.Location = new System.Drawing.Point(338, 93);
+            this.txtEuroxDolar.Name = "txtEuroxDolar";
+            this.txtEuroxDolar.Size = new System.Drawing.Size(70, 22);
+            this.txtEuroxDolar.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(430, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Peso";
+            // 
+            // txtEuroxPeso
+            // 
+            this.txtEuroxPeso.Location = new System.Drawing.Point(414, 95);
+            this.txtEuroxPeso.Name = "txtEuroxPeso";
+            this.txtEuroxPeso.Size = new System.Drawing.Size(70, 22);
+            this.txtEuroxPeso.TabIndex = 15;
+            // 
+            // txtDolarxPeso
+            // 
+            this.txtDolarxPeso.Location = new System.Drawing.Point(414, 137);
+            this.txtDolarxPeso.Name = "txtDolarxPeso";
+            this.txtDolarxPeso.Size = new System.Drawing.Size(70, 22);
+            this.txtDolarxPeso.TabIndex = 18;
+            // 
+            // txtDolarxDolar
+            // 
+            this.txtDolarxDolar.Location = new System.Drawing.Point(338, 140);
+            this.txtDolarxDolar.Name = "txtDolarxDolar";
+            this.txtDolarxDolar.Size = new System.Drawing.Size(70, 22);
+            this.txtDolarxDolar.TabIndex = 17;
+            // 
+            // TxtDolarxEuro
+            // 
+            this.TxtDolarxEuro.Location = new System.Drawing.Point(262, 137);
+            this.TxtDolarxEuro.Name = "TxtDolarxEuro";
+            this.TxtDolarxEuro.Size = new System.Drawing.Size(70, 22);
+            this.TxtDolarxEuro.TabIndex = 16;
+            // 
+            // TxtPesoXPeso
+            // 
+            this.TxtPesoXPeso.Location = new System.Drawing.Point(414, 183);
+            this.TxtPesoXPeso.Name = "TxtPesoXPeso";
+            this.TxtPesoXPeso.Size = new System.Drawing.Size(70, 22);
+            this.TxtPesoXPeso.TabIndex = 21;
+            // 
+            // txtPesoxDolar
+            // 
+            this.txtPesoxDolar.Location = new System.Drawing.Point(338, 183);
+            this.txtPesoxDolar.Name = "txtPesoxDolar";
+            this.txtPesoxDolar.Size = new System.Drawing.Size(70, 22);
+            this.txtPesoxDolar.TabIndex = 20;
+            // 
+            // txtPesoxEuro
+            // 
+            this.txtPesoxEuro.Location = new System.Drawing.Point(262, 183);
+            this.txtPesoxEuro.Name = "txtPesoxEuro";
+            this.txtPesoxEuro.Size = new System.Drawing.Size(70, 22);
+            this.txtPesoxEuro.TabIndex = 19;
+            // 
+            // SUBT
+            // 
+            this.SUBT.AutoSize = true;
+            this.SUBT.Location = new System.Drawing.Point(207, 38);
+            this.SUBT.Name = "SUBT";
+            this.SUBT.Size = new System.Drawing.Size(165, 16);
+            this.SUBT.TabIndex = 22;
+            this.SUBT.Text = "Conversiones de Moneda ";
+            // 
+            // Mostar
+            // 
+            this.Mostar.Location = new System.Drawing.Point(169, 233);
+            this.Mostar.Name = "Mostar";
+            this.Mostar.Size = new System.Drawing.Size(257, 29);
+            this.Mostar.TabIndex = 23;
+            this.Mostar.Text = "Mostar Historial de Conversiones";
+            this.Mostar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 274);
+            this.Controls.Add(this.Mostar);
+            this.Controls.Add(this.SUBT);
+            this.Controls.Add(this.TxtPesoXPeso);
+            this.Controls.Add(this.txtPesoxDolar);
+            this.Controls.Add(this.txtPesoxEuro);
+            this.Controls.Add(this.txtDolarxPeso);
+            this.Controls.Add(this.txtDolarxDolar);
+            this.Controls.Add(this.TxtDolarxEuro);
+            this.Controls.Add(this.txtEuroxPeso);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEuroxDolar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEuroxeuro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtConversion3);
+            this.Controls.Add(this.txtConversion2);
+            this.Controls.Add(this.btnConversion1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.txtEur);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TITULO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TITULO;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEur;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Button btnConversion1;
+        private System.Windows.Forms.Button txtConversion2;
+        private System.Windows.Forms.Button txtConversion3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEuroxeuro;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEuroxDolar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEuroxPeso;
+        private System.Windows.Forms.TextBox txtDolarxPeso;
+        private System.Windows.Forms.TextBox txtDolarxDolar;
+        private System.Windows.Forms.TextBox TxtDolarxEuro;
+        private System.Windows.Forms.TextBox TxtPesoXPeso;
+        private System.Windows.Forms.TextBox txtPesoxDolar;
+        private System.Windows.Forms.TextBox txtPesoxEuro;
+        private System.Windows.Forms.Label SUBT;
+        private System.Windows.Forms.Button Mostar;
+    }
+}
+
---
+++ Parcial 2/Parcial 2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parcial_2
+{
+    public partial class Form1 : Form
+    {
+        string connectionString =
+        @"Server=JOALEXS;Database=Cotizaciones;TrustServerCertificate=True;Integrated Security=True;";
+
+        const double Euro_a_dolar = 1.17;
+        const double Dolar_a_Peso = 4258.0;
+        const double euro_a_peso = Euro_a_dolar * Dolar_a_Peso;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnConversion1.Click += BtnConversion1_Click;
+            txtConversion2.Click += TxtConversion2_Click;
+            txtConversion3.Click += TxtConversion3_Click;
+            Mostar.Click += Mostar_Click;
+        }
+        private void GuardarConversion(string monedaOrigen, double cantidadOrigen, string monedaDestino, double cantidadDestino)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "INSERT INTO Conversiones (Fecha, MonedaOrigen, CantidadOrigen, MonedaDestino, CantidadDestino) " +
+                                   "VALUES (GETDATE(), @MonedaOrigen, @CantidadOrigen, @MonedaDestino, @CantidadDestino)";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@MonedaOrigen", monedaOrigen);
+                        cmd.Parameters.AddWithValue("@CantidadOrigen", cantidadOrigen);
+                        cmd.Parameters.AddWithValue("@MonedaDestino", monedaDestino);
+                        cmd.Parameters.AddWithValue("@CantidadDestino", cantidadDestino);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar en la base de datos: " + ex.Message);
+            }
+        }
+        private void BtnConversion1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double euro = double.Parse(txtEur.Text);
+                double dolar = euro * Euro_a_dolar;
+                double peso = euro * euro_a_peso;
+
+                txtEuroxeuro.Text = euro.ToString("0.00");
+                txtEuroxDolar.Text = dolar.ToString("0.00");
+                txtEuroxPeso.Text = peso.ToString("0.00");
+
+                GuardarConversion("Euro", euro, "Dólar", dolar);
+                GuardarConversion("Euro", euro, "Peso", peso);
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingrese un valor numérico válido en Euro.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TxtConversion2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double dolar = double.Parse(txtDolar.Text);
+                double euro = dolar / Euro_a_dolar;
+                double peso = dolar * Dolar_a_Peso;
+
+                TxtDolarxEuro.Text = euro.ToString("0.00");
+                txtDolarxDolar.Text = dolar.ToString("0.00");
+                txtDolarxPeso.Text = peso.ToString("0.00");
+
+                GuardarConversion("Dólar", dolar, "Euro", euro);
+                GuardarConversion("Dólar", dolar, "Peso", peso);
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingrese un valor numérico válido en Dólar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TxtConversion3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double peso = double.Parse(txtPeso.Text);
+                double dolar = peso / Dolar_a_Peso;
+                double euro = peso / euro_a_peso;
+
+                txtPesoxEuro.Text = euro.ToString("0.0000");
+                txtPesoxDolar.Text = dolar.ToString("0.0000");
+                TxtPesoXPeso.Text = peso.ToString("0.00");
+
+                GuardarConversion("Peso", peso, "Dólar", dolar);
+                GuardarConversion("Peso", peso, "Euro", euro);
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingrese un valor numérico válido en Peso.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Mostar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "SELECT Fecha, MonedaOrigen, CantidadOrigen, MonedaDestino, CantidadDestino FROM Conversiones ORDER BY Fecha DESC";
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (!reader.HasRows)
+                        {
+                            MessageBox.Show("No hay conversiones registradas aún.");
+                            return;
+                        }
+
+                        StringBuilder sb = new StringBuilder();
+                        while (reader.Read())
+                        {
+                            sb.AppendLine($"{reader["Fecha"]}: {reader["CantidadOrigen"]} {reader["MonedaOrigen"]} = {reader["CantidadDestino"]} {reader["MonedaDestino"]}");
+                        }
+
+                        MessageBox.Show(sb.ToString(), "Historial de Conversiones");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al mostrar el historial: " + ex.Message);
+            }
+        }
+    }
+}
