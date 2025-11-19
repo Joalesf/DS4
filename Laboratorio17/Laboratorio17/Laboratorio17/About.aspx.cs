8ffcb8fb5e5af34212231b438cf5c5e6a1784057
Joalesf <josealesanchez22@gmail.com>
2025-11-18 19:34:20 -0500

    Agregando LAB 17

---
+++ Laboratorio17/Laboratorio17/Laboratorio17/About.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.UI;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Laboratorio17
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ConnectionStringSettings connString = ConfigurationManager.ConnectionStrings["conexionNorthwind"];
+            SqlConnection conexion = new SqlConnection(connString.ConnectionString);
+
+            using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+                conexion.Open();
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        if (reader.Read())
+                        {
+                            GridV.DataSource = reader;
+                            GridV.DataBind();
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+}
