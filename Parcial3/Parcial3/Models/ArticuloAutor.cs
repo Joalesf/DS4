33b7dd02a93f41f47f43af11a895e52ac5d31a73
Joalesf <josealesanchez22@gmail.com>
2025-12-04 01:30:26 -0500

    Agregando Proyecto Parcial 3

---
+++ Parcial3/Parcial3/Models/ArticuloAutor.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial3.Models
+{
+    [Table("JS_ArticulosAutores")]
+    public class ArticuloAutor
+    {
+        [Key, Column(Order = 0)]
+        public int ArticuloId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int AutorId { get; set; }
+
+        public virtual Articulo Articulo { get; set; }
+        public virtual Autor Autor { get; set; }
+    }
+
+}
---
+++ Parcial3/Parcial3/Models/Autor.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Parcial3.Models
+{
+    [Table("JS_Autores")]
+    public class Autor
+    {
+        [Key]
+        public int AutorId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Apellido { get; set; }
+
+        public virtual ICollection<ArticuloAutor> ArticulosAutores { get; set; }
+
+        public Autor()
+        {
+            ArticulosAutores = new HashSet<ArticuloAutor>();
+        }
+    }
+}
